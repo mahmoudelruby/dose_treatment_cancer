f4d17d65fb708031f24d96ef504bf2bfa1667469
M.Sherief4 <mahmoudsherief31@gmail.com>
2022-05-13 23:24:14 +0200

    Google Excel Sheets linked

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:dose_treatment_cancer/modules/home_page/home_screen.dart';
-import 'package:dose_treatment_cancer/shared/cache_helper.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SharedPreferencesHelper.init();
+
 
   runApp(const MyApp());
 }
